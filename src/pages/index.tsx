130ad9cf4bba902b57adfaba08f26d706906ad41
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-07-11 09:01:28 +1000

    пп

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="https://getgems.io/fingerprints">
             <Translate>
             Collection on Getgems
           </Translate>
