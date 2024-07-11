255c9f3b61619a456f144ae261509e491e363749
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2024-07-11 11:28:36 +1000

    rm pwa

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -131,35 +131,4 @@ const config: Config = {
   } satisfies Preset.ThemeConfig,
 };
 
-export default {
-  plugins: [
-    [
-      "@docusaurus/plugin-pwa",
-      {
-        debug: true,
-        offlineModeActivationStrategies: [
-          "appInstalled",
-          "standalone",
-          "queryString",
-        ],
-        pwaHead: [
-          {
-            tagName: "link",
-            rel: "icon",
-            href: "/img/docusaurus.png",
-          },
-          {
-            tagName: "link",
-            rel: "manifest",
-            href: "/manifest.json",
-          },
-          {
-            tagName: "meta",
-            name: "theme-color",
-            content: "rgb(37, 194, 160)",
-          },
-        ],
-      },
-    ],
-  ],
-};
+export default config;
